91648f6793628dac4f106d682cebca421afba206
matt trask <mjftrask@gmail.com>
2017-06-04 10:05:24 -0500

    whooops!

--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -1,5 +1,5 @@
 import VueRouter from 'vue-router';
-import Home from './components/Home.vue';
+import Home from './components/home.vue';
 import About from './components/about.vue';
 
 const routes = [
