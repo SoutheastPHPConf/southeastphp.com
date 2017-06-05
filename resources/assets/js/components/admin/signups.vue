5f7621fdf8a4873e75ef7de052fac2f7c8c6d3f4
matt trask <mjftrask@gmail.com>
2017-06-04 22:05:32 -0500

    template to see all the emails

---
+++ resources/assets/js/components/admin/signups.vue
@@ -0,0 +1,67 @@
+<style lang='scss'>
+ .container {
+     padding-top: 10rem;
+ }
+</style>
+
+<template>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <table class="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Email</th>
+                            <th>Signup Date</th>
+                            <th>Duplicate</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="email in emails">
+                            <td> {{ email.email }}</td>
+                            <td> {{ email.signupDate.date }}</td>
+                            <td>
+                                <label>
+                                    <input type="checkbox"> Duplicate
+                                </label>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+</template>
+<script>
+  import axios from 'axios';
+
+  export default {
+
+    mounted() {
+      this.getEmailSignups();
+    },
+
+    computed: {
+
+    },
+
+    data()
+    {
+      return {
+        emails: [],
+      };
+    },
+
+    methods: {
+      getEmailSignups() {
+        return axios.get('api/emails').then((response) => {
+          this.emails = response.data.data;
+        }).catch((error) => {
+          //
+        });
+      },
+    },
+  };
+</script>
