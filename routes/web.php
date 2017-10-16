f936b4c220a79b6f9f38bbf3d2ddc76170a994c2
Adewale Adeleye <Adewagold@yahoo.com>
2017-10-16 13:57:56 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,14 @@ Route::get('/', function () {
     return view('home');
 });
 
+
+Route::get('login/facebook', 'AuthController@redirectToFacebook');
+Route::get('login/facebook/callback', 'AuthController@getFacebookCallback');
+Route::get('login/google', 'AuthController@redirectToGoogle');
+Route::get('login/google/callback', 'AuthController@getGoogleCallback');
+Route::get('/complete-social',[
+	'uses'=> 'AuthController@completeSocial',
+	'as'=>'auth.signup']
+	);
+
 Auth::routes();
