1ddc94b28ebb9cecff7ebcee6c47030837d70610
matt trask <mjftrask@gmail.com>
2017-12-17 11:49:39 -0600

    header and footer components

---
+++ resources/assets/js/components/footer.vue
@@ -0,0 +1,15 @@
+<style lang='scss'>
+
+</style>
+<template>
+    <div>
+        <footer>
+            <p class="text-center text-light">Copyright 2017 SoutheastPHP</p>
+        </footer>
+    </div>
+</template>
+<script>
+  import axios from 'axios';
+
+  export default {};
+</script>
