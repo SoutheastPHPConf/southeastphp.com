ab50722d6a920d3397d461d5c0473e4533f50639
matt trask <mjftrask@gmail.com>
2017-12-28 14:13:12 -0500

    login controller work

---
+++ app/Transformers/Api/UserTransformer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Transformers\Api;
+
+use League\Fractal\TransformerAbstract;
+
+class UserTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform()
+    {
+        return [
+            //
+        ];
+    }
+}
