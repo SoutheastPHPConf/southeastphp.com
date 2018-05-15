82234c31f03162f8d6d89a6393443d056001eec1
matt trask <mjftrask@gmail.com>
2018-05-14 21:58:42 -0500

    added stuff to do in nashville

--- resources/assets/js/components/venue.vue
+++ resources/assets/js/components/venue.vue
@@ -21,25 +21,58 @@
                     </div>
                     <div class="row">
                         <div class="col-xs-12 padding-top">
-                            <p>We picked the Hotel Preston for its location in Nashville. Less then a 10 minute drive to the airport, so no one has to go to far, yet right outside of the main downtown area.
+                            <p class="tracking-wide">We picked the Hotel Preston for its location in Nashville. Less then a 10 minute drive to the airport, so no one has to go to far, yet right outside of the main downtown area.
                                 It has been recently remodeled and we can not wait to show you. They have a convenient shuttle that runs downtown in case you want to explore the music and sights of Nashville, and you
                                 are also minutes away from one of Nashville's most history sites: The Grand Ole Opry.
                             </p>
-                            <p>Some of the things you can do while at the Hotel Preston:</p>
+                            <p class="tracking-wide">Some of the things you can do while at the Hotel Preston:</p>
                             <ul>
-                                <li>24 hour gym</li>
+                                <li class="tracking-wide">24 hour gym</li>
                                 <li>Listen to live music in their lounge</li>
-                                <li>Be 20 minutes away from downtown and 10 minutes away from the airport</li>
+                                <li class="tracking-wide">Be 20 minutes away from downtown and 10 minutes away from the airport</li>
                             </ul>
-                            <p>In order to help our attendees out, we have got a nice little deal with the hotel to get a discount on a room! The best part about this code is that it does not expire. Use it today, tomorrow or the day before the conference (rooms permitting of course).</p>
-                            <p>To book, <a href="https://tinyurl.com/SEPHPPreston"><button class="btn button-red">click here!</button></a> and use the code SOUTHEAST. This is good till the day of the conference, so take advantage of it!</p>
+                            <p class="tracking-wide">In order to help our attendees out, we have got a nice little deal with the hotel to get a discount on a room! The best part about this code is that it does not expire. Use it today, tomorrow or the day before the conference (rooms permitting of course).</p>
+                            <p class="tracking-wide">To book, <a href="https://tinyurl.com/SEPHPPreston"><button class="btn button-red">click here!</button></a> and use the code SOUTHEAST. This is good till the day of the conference, so take advantage of it!</p>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-xs-12 padding-top">
                             <h3>Nashville, Tennessee</h3>
                             <p>There is just so much to do in Nashville. From history to nature to music, you can throw a rock and hit something interesting to do!</p>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="well">
+                                        <h2 class="tracking-wide font-sans-narrow">Food and Drink</h2>
+                                        <p class="tracking-wide">We promise more then just hot chicken.</p>
+                                        <p><a href="http://www.butchertownhall.com/">Butchertown Hall</a> - <a href="https://www.yelp.com/biz/butchertown-hall-nashville?osq=butchertown+hall">Yelp</a> - <a href="https://www.google.com/search?source=hp&ei=T1T3Wva8FMfHjwSj87vQDw&q=butchertown+hall&oq=butchertow&gs_l=psy-ab.3.0.35i39k1j0i20i264k1j0l8.141698.144737.0.145359.20.14.3.0.0.0.177.1506.7j6.13.0..2..0...1.1.64.psy-ab..4.16.1518.0..46j0i67k1j0i131k1j0i20i264i46k1j46i20i264k1j0i20i263i264k1j0i46k1j0i10k1j0i10i46k1j46i10k1.0.5fK6aMll1cQ#lrd=0x88646703ae8d5811:0xe6537aa47c217248,1,,,">Google</a> | "Soulful, coal-roasted plates and Delectable oak-smoked meats in an inspiring setting."</p>
+                                        <p><a href="http://hattieb.com/">Hattie B's</a> - <a href="https://www.yelp.com/biz/hattie-bs-chicken-midtown-nashville-2?osq=hattie+b%3Bs">Yelp</a> - <a
+                                                href="https://www.google.com/search?ei=4VT3WrD2HMWYjwSXmqXwAw&q=hattie%20b%27s&oq=hattie+b%27s&gs_l=psy-ab.3..0l2j0i20i263k1l2j0l6.791674.794306.0.794456.12.11.1.0.0.0.146.1100.7j4.11.0....0...1.1.64.psy-ab..0.12.1102...35i39k1j0i67k1j0i46i67k1j46i67k1j0i131k1j0i20i264k1j0i131i20i264k1j0i10k1.0.tV4_jLyVCGQ&npsic=0&rflfq=1&rlha=0&rllag=36140280,-86814186,3515&tbm=lcl&rldimm=11608428533289486235&ved=0ahUKEwjowqvSioHbAhXG34MKHR2DAKwQvS4ITjAB&rldoc=1&tbs=lrf:!2m1!1e3!3sIAE,lf:1,lf_ui:4#lrd=0x886466938a02b449:0xa1196c3f373a879b,1,,,&rlfi=hd:;si:11608428533289486235;mv:!1m3!1d2577471.0762644056!2d-86.29418635!3d35.88296725!2m3!1f0!2f0!3f0!3m2!1i57!2i299!4f13.1;tbs:lrf:!2m1!1e3!3sIAE,lf:1,lf_ui:4">Google</a> | One of the most popular Hot Chicken places in Nashville. Tip: order it online and either have it delivered or pick it up. Waiting in line sucks.</p>
+                                        <p><a href="https://fivepointspizza.com/">Five Points Pizza</a> - <a href="https://www.yelp.com/biz/five-points-pizza-nashville?osq=five+points">Yelp</a> - <a
+                                                href="https://www.google.com/search?q=five+points+pizza&rlz=1C5CHFA_enUS721US721&oq=five+points+pizza&aqs=chrome..69i57j69i60j0l4.2991j1j1&sourceid=chrome&ie=UTF-8#lrd=0x886467d294e1c6dd:0xb7f24e896b575ffe,1,,,">Google</a> | A favorite pizza place for locals, you can do slices or whole pizzas here with a solid beer list.</p>
+                                        <p><a href="http://thewildcow.com/">Wild Cow</a> - <a href="https://www.yelp.com/biz/the-wild-cow-nashville-2?osq=wild+cow">Yelp</a> - <a
+                                                href="https://www.google.com/search?q=wild+cow&rlz=1C5CHFA_enUS721US721&oq=wild+cow&aqs=chrome.0.69i59j0l2j69i57j69i60l2.791j0j4&sourceid=chrome&ie=UTF-8#lrd=0x8864687bb88b413b:0xdd19c7a3be21e109,1,,,">Google</a> | A local vegetarian/vegan favorite located in the hip area of east Nashville, its surrounded by quaint coffee shops and other fun little shops to enjoy a nice walk around!</p>
+                                    </div>
 
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="well">
+                                        <h2 class="tracking-wide font-sans-narrow">Nature</h2>
+                                        <p class="tracking-wide">Places to get a break from the computer.</p>
+                                        <p><a href="http://www.nashville.gov/Parks-and-Recreation/Greenways-and-Trails.aspx">Metro Greenways</a> | Nashville has a ton of greenspace across town. If you want, you can rent a B-cycle or Ofo and ride along the Cumberland River. If you ask Matt, he may take you on a tour one of the days you are here!</p>
+                                        <p><a href="http://www.nashville.gov/Parks-and-Recreation/Parthenon.aspx">The Parthenon</a> | A full scale replica of the Parthenon in Greece, it contains an art museum as well as expansive greenways which is perfect for a day away from the computer.</p>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="well">
+                                        <h2 class="tracking-wide font-sans-narrow">Music</h2>
+                                        <p class="tracking-wide">We are Music City after all.</p>
+                                        <p><a href="https://www.ryman.com/">The Ryman</a> | The original home of the Grand Ole Opry. If you catch a show, try to get tickets to the balcony. You can tour the Ryman as well and even record your very own record. We won't promise it;ll sound good though.</p>
+                                        <p><a href="https://www.opry.com/">The Opry</a> | A must see if you are a country fan. Catch beginning acts or huge national acts at any given time here, and even take a tour!</p>
+                                        <p><a href="https://www.exitin.com/">Exit/In</a> | The sarcastic soul of Nashville's rock scene.</p>
+                                        <p><a href="https://www.marathonmusicworks.com/">Marathon Music Works</a> | From hip hop to metal to country jams, this place covers it all in a smaller venue. Settled by the old Marathon Motorworks factory, you will never see a bad show here.</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
